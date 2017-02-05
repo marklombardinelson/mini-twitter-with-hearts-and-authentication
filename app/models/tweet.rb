56e8b5aae42544a8844558fe63c6f4c136086a01
MarkLombardiNelson <marklombardinelson@gmail.com>
2017-02-04 23:37:05 -0500

    Migrating User Id, creating logout, showing email address, and hearting multiple tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord
+  belongs_to :user
   has_many :hearts
   validates :post, length: { maximum: 140 }, presence: true
   validates :username, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bob = User.create!(email: "mark@example.com", password: "12345678")
+jayz = User.create!(email: "jayz@example.com", password: "123456789")
 tweets = Tweet.create(
 [
-  { post: "Today I would like each of us to reflect on his and her own past and the gifts received from the Lord.", username: "Pope Francis" },
-  { post: "How to land your first job in tech, with advice by @samkap and @trimblyton http://bit.ly/2hrqjxw ", username: "The Iron Yard" },
-  { post: "With SQL Server, you get everything you need.", username: "Microsoft SQL Server" }
+  { post: "Today I would like each of us to reflect on his and her own past and the gifts received from the Lord.", username: "Pope Francis", user: bob },
+  { post: "How to land your first job in tech, with advice by @samkap and @trimblyton http://bit.ly/2hrqjxw ", username: "The Iron Yard", user: bob },
+  { post: "With SQL Server, you get everything you need.", username: "Microsoft SQL Server", user: bob },
+  { post: "It's Ya Boiiii.", username: "JZ", user: jayz }
   ]
   )
